de76e685623abd7d0fe25cbf63f6c2e942a34bde
r-k-baral <160538972+r-k-baral@users.noreply.github.com>
2026-06-22 17:12:10 +0530

    add  a card component

---
+++ foodres/src/Res-card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import dish from'./assets/dish.jpg'
+import"./Res-card.css"
+
+const ResCard = () => {
+  return (
+    <div>
+      
+      <div className='cardtop'>
+        <img src={dish} alt=" south indian food"  className='img-card'/>
+        <h3>South Indian</h3>
+        <h4>🌟 🌟 🌟</h4>
+        <p>Enjoy the rich flavors of South India with traditional dishes like masala dosa, idli, vada, uttapam, and aromatic filter coffee</p>
+         <button className='prize'>₹. 300</button>
+
+      </div>
+    </div>
+  )
+}
+
+export default ResCard
