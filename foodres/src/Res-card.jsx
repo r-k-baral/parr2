90b456b5864c963c8d49570958ed13993b084211
r-k-baral <160538972+r-k-baral@users.noreply.github.com>
2026-06-22 18:18:53 +0530

    update the card css

--- foodres/src/Res-card.jsx
+++ foodres/src/Res-card.jsx
@@ -9,8 +9,13 @@ const ResCard = () => {
       <div className='cardtop'>
         <img src={dish} alt=" south indian food"  className='img-card'/>
         <h3>South Indian</h3>
-        <h4>🌟 🌟 🌟</h4>
+        <h4>🌟 🌟 🌟 🌟</h4>
         <p>Enjoy the rich flavors of South India with traditional dishes like masala dosa, idli, vada, uttapam, and aromatic filter coffee</p>
+        <div className='time'>
+            <h5>🌟 4.4 </h5>
+            <h5>🕛38 min</h5>
+            <h5>🟢 pure veg </h5>
+        </div>
          <button className='prize'>₹. 300</button>
 
       </div>
